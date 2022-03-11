cb1fd1ea4abc170730b2b1b552c23a7123be93e0
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2022-03-11 18:38:04 +0100

    makor updates

--- scripts/run.js
+++ scripts/run.js
@@ -5,7 +5,6 @@ const main = async () => {
   await domainContract.deployed();
 
   console.log("Contract deployed to:", domainContract.address);
-  console.log("Contract deployed by:", owner.address);
 
   // We're passing in a second variable - value. This is the moneyyyyyyyyyy
   let txn = await domainContract.register("lekandev",  {value: hre.ethers.utils.parseEther('0.1')});
